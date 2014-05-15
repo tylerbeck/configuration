3bb90c398fae9690391d68519239df8f2ed00774
Tyler Beck <tyler@tylerbeck.com>
2014-05-15 09:58:17 -0400

    added comments

--- assets/grunt/tasks/manage-hosts.js
+++ assets/grunt/tasks/manage-hosts.js
@@ -22,12 +22,16 @@ module.exports = function( grunt ){
 	//temp path holder
 	var projectPath = "";
 
+	/**
+	 * options object
+	 */
 	var options = {};
 
 	//prompt properties
 	var ipv4RegEx = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 	var ipv6RegEx = /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/;
     var domainRegEx = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*(\.[a-z]{2,99})?$/;
+
 	var questions ={
 		domain: {
 			message: "domain",
@@ -248,6 +252,14 @@ module.exports = function( grunt ){
 		}
 	};
 
+	/**
+	 * utility method for 'when' methods in prompts
+	 * @param prop
+	 * @param value
+	 * @param answers
+	 * @param negate
+	 * @returns {boolean}
+	 */
 	function matchFn( prop, value, answers, negate ){
 		var match = options[prop] == value || answers[prop] == value;
 		return negate ? !match : match;
