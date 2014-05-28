4cfbd57934741d98bd3197c55296e3c6b7a887ff
Tyler Beck <tyler@tylerbeck.com>
2014-05-28 16:41:22 -0400

    added aliases for project configuration commands

--- conf.default/bash/profile/aliases.sh
+++ conf.default/bash/profile/aliases.sh
@@ -2,3 +2,15 @@
 	alias ls='ls -a'
 
 #CUSTOM ALIASES ---------------------------------
+
+
+#PROJECT CONFIGURATION ALIASES ------------------
+	alias projectcreate='CWD=$(pwd) && cd ~/Projects/configuration/ && grunt create-project && cd $CWD'
+
+	alias vhostadd='CWD=$(pwd) && cd ~/Projects/configuration/ && grunt add-vhost && cd $CWD'
+
+	alias vhostremove='CWD=$(pwd) && cd ~/Projects/configuration/ && grunt remove-vhost && cd $CWD'
+
+	alias hostsadd='CWD=$(pwd) && cd ~/Projects/configuration/ && grunt add-hosts-entry && cd $CWD'
+
+	alias hostsremove='CWD=$(pwd) && cd ~/Projects/configuration/ && grunt remove-hosts-entry && cd $CWD'
